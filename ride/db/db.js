3e44c15b072947581696f59e02779b501df6cc3e
souvikbiswas2k3-cloud <souvikbytecodes@gmail.com>
2025-08-26 15:41:36 +0530

    Ride Service Created and All Services are Connected to RabbitMQ

---
+++ ride/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+function connect() {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log('ride service connected to MongoDB');
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+
+module.exports = connect;
